d0fe4960dfcc3f337cd8aae3694cac23dd5dfd34
YuriDevAT <JUndeutsch@gmx.at>
2021-02-05 21:14:08 +0100

    rearranged code, will working with props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
+import { useState } from 'react';
 import Form from './components/Form';
 import ToDoList from './components/ToDoList';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState('');
+
   return (
     <div className="App">
       <header>
-        <h1>Julias To Do list</h1>
+        <h3 className="display-5">bootstrap</h3>  
+        <h1 className="display-3">To Do List</h1>          
       </header>
-      <Form />
-     <ToDoList />
+      <Form 
+      input={input}
+      setInput={setInput}
+      todos={todos}
+      setTodos={setTodos}
+      />
+     <ToDoList todos={todos} />
     </div>
   );
 }
