952f50b0ac3075be9e8650baf8dcb33781457858
YuriDevAT <JUndeutsch@gmx.at>
2021-02-05 21:15:46 +0100

    filled component with input

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const ToDo = () => {
-    
+const ToDo = ({ text }) => {    
 
     return (
         <div>
-
+            <li>{text}</li>
+            <button><i className="bi bi-check2-square"></i></button>
+            <button><i className="bi bi-trash"></i></button>
         </div>
-
     );
-}
+};
 
 export default ToDo;
