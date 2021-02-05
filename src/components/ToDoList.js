c13676b81efab71dbe74a2da2abb0833e23b27ce
YuriDevAT <JUndeutsch@gmx.at>
2021-02-05 21:13:08 +0100

    added .map function to render every single todo input

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ToDo from './ToDo';
 
-const ToDoList = () => {
+const ToDoList = ({ todos }) => {
 
     return (
         <div className="to-do-list">
             <ul>
-                {todos.map((todo) => (
-                    <ToDo />
-                ))}                
+                {todos.map(todo => (
+                    <ToDo text={todo.text} />
+                ))}
             </ul>
         </div>
-    )
-}
+    );
+};
 
 export default ToDoList;
